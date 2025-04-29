32e727f9fe745f10bfc76cb6b94120190c2b1380
KrajncAndraz <andraz.krajnc1@student.um.si>
2025-04-29 11:23:42 +0200

    Implement user registration

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,17 +1,33 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
-var Schema   = mongoose.Schema;
+var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	'username' : String,
-	'password' : String,
-	'email' : String
+	'username': {
+		type: String,
+		required: [true, 'Username is required'],
+		unique: true,
+		trim: true
+	},
+	'password': {
+		type: String,
+		required: [true, 'Password is required'],
+		minlength: [6, 'Password must be at least 6 characters long']
+	},
+	'email': {
+		type: String,
+		required: [true, 'Email is required'],
+		unique: true,
+		trim: true,
+		lowercase: true
+	}
 });
 
-userSchema.pre('save', function(next){
+userSchema.pre('save', function (next) {
 	var user = this;
-	bcrypt.hash(user.password, 10, function(err, hash){
-		if(err){
+	if (!user.isModified('password')) return next(); // Only hash if password is modified
+	bcrypt.hash(user.password, 10, function (err, hash) {
+		if (err) {
 			return next(err);
 		}
 		user.password = hash;
@@ -19,26 +35,25 @@ userSchema.pre('save', function(next){
 	});
 });
 
-userSchema.statics.authenticate = function(username, password, callback){
-	User.findOne({username: username})
-	.exec(function(err, user){
-		if(err){
-			return callback(err);
-		} else if(!user) {
-			var err = new Error("User not found.");
-			err.status = 401;
-			return callback(err);
-		} 
-		bcrypt.compare(password, user.password, function(err, result){
-			if(result === true){
-				return callback(null, user);
-			} else{
-				return callback();
+userSchema.statics.authenticate = function (username, password, callback) {
+	User.findOne({ username: username })
+		.exec(function (err, user) {
+			if (err) {
+				return callback(err);
+			} else if (!user) {
+				var err = new Error("User not found.");
+				err.status = 401;
+				return callback(err);
 			}
+			bcrypt.compare(password, user.password, function (err, result) {
+				if (result === true) {
+					return callback(null, user);
+				} else {
+					return callback();
+				}
+			});
 		});
-		 
-	});
-}
+};
 
 var User = mongoose.model('user', userSchema);
 module.exports = User;
